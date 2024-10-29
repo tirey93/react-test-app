02006931d9dbb6ef7af36b8bc7b55cd32c3b99de
tirey93 <tirey93@gmail.com>
2024-10-29 12:53:27 +0100

    added dialog

---
+++ src/components/DialogMine.tsx
@@ -0,0 +1,88 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+interface DialogMineProps {
+    open: boolean;
+    handleClose: () => void;
+    currentId: number;
+}
+
+interface DialogMineState {
+    titles: Map<number, string>
+}
+
+class DialogMine extends React.Component<DialogMineProps, DialogMineState> {
+    constructor(props: DialogMineProps) {
+        super(props);
+        this.state = {
+            titles: new Map([
+                [1, "title1"],
+                [2, "title2"],
+                [3, "title3"],
+            ])
+        };
+    }
+
+    render() {
+        return (
+            <>
+                <Dialog
+                    fullWidth
+                    maxWidth="xs"
+                    open={this.props.open}
+                    onClose={this.props.handleClose}
+                    PaperProps={{
+                        component: 'form',
+                        onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                            event.preventDefault();
+                            const formData = new FormData(event.currentTarget);
+                            const formJson = Object.fromEntries((formData as any).entries());
+                            const email = formJson.email;
+                            console.log(formJson);
+                            this.props.handleClose();
+                        },
+                    }}
+                >
+                    <DialogTitle>Editing</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Editing user nr: {this.props.currentId}
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            required
+                            margin="dense"
+                            id="name"
+                            name="email"
+                            label="Email Address"
+                            type="email"
+                            fullWidth
+                            variant="standard"
+                        />
+                        <TextField
+                            autoFocus
+                            required
+                            margin="dense"
+                            id="title"
+                            name="title"
+                            label="Title"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            value={this.state.titles.get(this.props.currentId)}
+                            onChange={(event) => {
+                                 return this.setState({titles: this.state.titles.set(this.props.currentId, event.target.value)});
+                            }}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.props.handleClose}>Cancel</Button>
+                        <Button type="submit">Subscribe</Button>
+                    </DialogActions>
+                </Dialog>
+            </>
+        );
+    }
+}
+
+export default DialogMine;
--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -1,6 +1,7 @@
 import { IconButton, Menu, MenuItem, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DialogMine from "./DialogMine";
 
 const Fetch = () => {
     interface Post {
@@ -8,52 +9,54 @@ const Fetch = () => {
         id: number;
         title: string;
         body: string;
-      }
-      
+    }
+
     const [data, setData] = useState<Post[] | null>(null);
     const [loading, setLoading] = useState(true);
+    const [openDialog, setOpenDialog] = useState(false);
     const [error, setError] = useState("");
     const [currentId, setCurrentId] = useState(0);
 
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>, id: number) => {
-      setAnchorEl(event.currentTarget);
-      setCurrentId(id);
+        setAnchorEl(event.currentTarget);
+        setCurrentId(id);
     };
     const handleClose = () => {
-      console.log("on close", currentId);
-      setAnchorEl(null);
+        console.log("on close", currentId);
+        setAnchorEl(null);
+        setOpenDialog(true);
     };
-  
+
     useEffect(() => {
-      const fetchDataForPosts = async () => {
-        try {
-          const response = await fetch(
-            `https://jsonplaceholder.typicode.com/posts?_limit=8`
-          );
-          if (!response.ok) {
-            throw new Error(`HTTP error: Status ${response.status}`);
-          }
-          let postsData = await response.json();
-          setData(postsData);
-          setError("");
-        } catch (err) {
-          if(err instanceof Error){
-              setError(err.message);
-          }
-          setData(null);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchDataForPosts();
+        const fetchDataForPosts = async () => {
+            try {
+                const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/posts?_limit=8`
+                );
+                if (!response.ok) {
+                    throw new Error(`HTTP error: Status ${response.status}`);
+                }
+                let postsData = await response.json();
+                setData(postsData);
+                setError("");
+            } catch (err) {
+                if (err instanceof Error) {
+                    setError(err.message);
+                }
+                setData(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDataForPosts();
     }, []);
-    return ( 
+    return (
         <>
             {loading && (
-            <div className="text-xl font-medium">Loading posts...</div>
+                <div className="text-xl font-medium">Loading posts...</div>
             )}
             {error && <div className="text-red-700">{error}</div>}
             {
@@ -61,33 +64,33 @@ const Fetch = () => {
                     <TableContainer component={Paper}>
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
                             <TableHead>
-                            <TableRow>
-                                <TableCell>User Id</TableCell>
-                                <TableCell>Id</TableCell>
-                                <TableCell>Title</TableCell>
-                                <TableCell>Body</TableCell>
-                            </TableRow>
+                                <TableRow>
+                                    <TableCell>User Id</TableCell>
+                                    <TableCell>Id</TableCell>
+                                    <TableCell>Title</TableCell>
+                                    <TableCell>Body</TableCell>
+                                </TableRow>
                             </TableHead>
                             <TableBody>
-                            {data.map((row) => (
-                                <TableRow
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                <TableCell>{row.userId}</TableCell>
-                                <TableCell>{row.id}</TableCell>
-                                <TableCell>{row.title}</TableCell>
-                                <TableCell>{row.body}</TableCell>
-                                <TableCell>
-                                    <IconButton
-                                        id="long-button"
-                                        onClick={(event) => handleClick(event, row.id)}
+                                {data.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
-                                        <MoreVertIcon />
-                                    </IconButton>
-                                </TableCell>
-                                </TableRow>
-                            ))}
+                                        <TableCell>{row.userId}</TableCell>
+                                        <TableCell>{row.id}</TableCell>
+                                        <TableCell>{row.title}</TableCell>
+                                        <TableCell>{row.body}</TableCell>
+                                        <TableCell>
+                                            <IconButton
+                                                id="long-button"
+                                                onClick={(event) => handleClick(event, row.id)}
+                                            >
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
                             </TableBody>
                         </Table>
 
@@ -101,11 +104,15 @@ const Fetch = () => {
                             <MenuItem onClick={handleClose}>Action</MenuItem>
                             <MenuItem onClick={handleClose}>Remove</MenuItem>
                         </Menu>
+                        <DialogMine
+                            open={openDialog}
+                            handleClose={() => setOpenDialog(false)}
+                            currentId={currentId} />
                     </TableContainer>
                 )
             }
         </>
-     );
+    );
 }
- 
+
 export default Fetch;
